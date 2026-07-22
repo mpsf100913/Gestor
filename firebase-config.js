adadfe0a91f85f9428eeed2dddee1a2b9a9d71a8
mpsf100913 <marcospaulofogaca@gmail.com>
2026-07-22 00:09:07 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,15 +7,19 @@
 const firebaseConfig = {
   apiKey: "AIzaSyB5Y2BMqHIBDAhZoHFwDzdG-UuvPR1-FcQ",
   authDomain: "gestor-22b13.firebaseapp.com",
-  databaseURL: "https://gestor-22b13-default-rtdb.firebaseio.com", // ex: https://iptv-cobranca-default-rtdb.firebaseio.com
+  databaseURL: "https://gestor-22b13-default-rtdb.firebaseio.com",
   projectId: "gestor-22b13",
   storageBucket: "gestor-22b13.firebasestorage.app",
   messagingSenderId: "124521158946",
   appId: "1:124521158946:web:06ea51762e01c571512d7b"
 };
 
+// VAPID Key para Push Notifications (Cloud Messaging)
+// Gerar em: Firebase Console > Configurações > Cloud Messaging > Certificado de Servidor Web
+const VAPID_KEY = "BH4XFOg4RddXU-xPuEh3Loli2QiGk1XuXhX4K9jmDajsdMf3DkfgLJUcZ6Rhy62-KjZ7FKpbGyiS5YnTwUA5D9w";
+
 // URL base do Realtime Database (usada nas chamadas REST via fetch/PATCH)
 const DB_URL = firebaseConfig.databaseURL;
 
 // PIN de acesso ao painel admin (troque por um número só seu)
-const ADMIN_PIN = "891322";
+const ADMIN_PIN = "0000";
